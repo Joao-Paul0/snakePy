ff57242a49cca3edd328256f5570b2a361a2f6cf
Joao-Paul0 <joaopaulo.tomazaquino@hotmail.com>
2022-05-14 22:25:45 -0300

    Update main

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,10 +1,10 @@
-#importando bibliotecas
+# importando bibliotecas
 import turtle
 import random
 import time
 
 
-#criando turtle screen
+# criando turtle screen
 screen = turtle.Screen()
 screen.title('Joao-Paul0')
 screen.setup(width = 700, height = 700)
@@ -13,7 +13,7 @@ turtle.bgcolor('#FFF')
 
 
 
-#criando um border para o nosso game
+# criando um border para o nosso game
 
 turtle.speed(5)
 turtle.pensize(4)
@@ -31,136 +31,136 @@ turtle.forward(500)
 turtle.penup()
 turtle.hideturtle()
 
-#ponto
-score = 0
+# ponto
+pontos = 0
 delay = 0.1
 
 
-#snake
-snake = turtle.Turtle()
-snake.speed(0)
-snake.shape('square')
-snake.color("black")
-snake.penup()
-snake.goto(0,0)
-snake.direction = 'stop'
+# cobra
+cobra = turtle.Turtle()
+cobra.speed(0)
+cobra.shape('square')
+cobra.color("black")
+cobra.penup()
+cobra.goto(0,0)
+cobra.direcao = 'stop'
 
 
-#comida
-fruit = turtle.Turtle()
-fruit.speed(0)
-fruit.shape('circle')
-fruit.color('green')
-fruit.penup()
-fruit.goto(30,30)
+# comida
+fruta = turtle.Turtle()
+fruta.speed(0)
+fruta.shape('circle')
+fruta.color('green')
+fruta.penup()
+fruta.goto(30,30)
 
-old_fruit=[]
+velha_fruta=[]
 
-#pontuação
-scoring = turtle.Turtle()
-scoring.speed(0)
-scoring.color("black")
-scoring.penup()
-scoring.hideturtle()
-scoring.goto(0,300)
-scoring.write("Score :",align="center",font=("Courier",24,"bold"))
+# pontuação
+pontuacao = turtle.Turtle()
+pontuacao.speed(0)
+pontuacao.color("black")
+pontuacao.penup()
+pontuacao.hideturtle()
+pontuacao.goto(0,300)
+pontuacao.write("Score :",align="center",font=("Courier",24,"bold"))
 
 
-#######definindo os movimentos
-def snake_go_up():
-    if snake.direction != "down":
-        snake.direction = "up"
+####### definindo os movimentos
+def cobra_indo_pra_cima():
+  if cobra.direcao != "down":
+    cobra.direcao = "up"
 
-def snake_go_down():
-    if snake.direction != "up":
-        snake.direction = "down"
+def cobra_indo_pra_baixo():
+  if cobra.direcao != "up":
+    cobra.direcao = "down"
 
-def snake_go_left():
-    if snake.direction != "right":
-        snake.direction = "left"
+def cobra_indo_pra_esquerda():
+  if cobra.direcao != "right":
+    cobra.direcao = "left"
 
-def snake_go_right():
-    if snake.direction != "left":
-        snake.direction = "right"
+def cobra_indo_pra_direita():
+  if cobra.direcao != "left":
+    cobra.direcao = "right"
 
-def snake_move():
-    if snake.direction == "up":
-        y = snake.ycor()
-        snake.sety(y + 20)
+def cobra_move():
+  if cobra.direcao == "up":
+    y = cobra.ycor()
+    cobra.sety(y + 20)
 
-    if snake.direction == "down":
-        y = snake.ycor()
-        snake.sety(y - 20)
+  if cobra.direcao == "down":
+    y = cobra.ycor()
+    cobra.sety(y - 20)
 
-    if snake.direction == "left":
-        x = snake.xcor()
-        snake.setx(x - 20)
+  if cobra.direcao == "left":
+    x = cobra.xcor()
+    cobra.setx(x - 20)
 
-    if snake.direction == "right":
-        x = snake.xcor()
-        snake.setx(x + 20)
+  if cobra.direcao == "right":
+    x = cobra.xcor()
+    cobra.setx(x + 20)
 
 # mapeando as teclas
 screen.listen()
-screen.onkeypress(snake_go_up, "Up")
-screen.onkeypress(snake_go_down, "Down")
-screen.onkeypress(snake_go_left, "Left")
-screen.onkeypress(snake_go_right, "Right")
+screen.onkeypress(cobra_indo_pra_cima, "Up")
+screen.onkeypress(cobra_indo_pra_baixo, "Down")
+screen.onkeypress(cobra_indo_pra_esquerda, "Left")
+screen.onkeypress(cobra_indo_pra_direita, "Right")
 
-#main loop
+# main loop
 
 while True:
   screen.update()
       #colisão da cobra e as frutas
-  if snake.distance(fruit)< 20:
-          x = random.randint(-290,270)
-          y = random.randint(-240,240)
-          fruit.goto(x,y)
-          scoring.clear()
-          score+=1
-          scoring.write("Score:{}".format(score),align="center",font=("Courier",24,"bold"))
-          delay-=0.001
-          
-          ## criando uma nova bola
-          new_fruit = turtle.Turtle()
-          new_fruit.speed(0)
-          new_fruit.shape('square')
-          new_fruit.color('green')
-          new_fruit.penup()
-          old_fruit.append(new_fruit)
-          
+  if cobra.distance(fruta)< 20:
+    x = random.randint(-290,270)
+    y = random.randint(-240,240)
+    fruta.goto(x,y)
+    pontuacao.clear()
+    pontos+=1
+    pontuacao.write("Score:{}".format(pontos),align="center",font=("Courier",24,"bold"))
+    delay-=0.001
+    
+    ## criando uma nova bola
+    nova_fruta = turtle.Turtle()
+    nova_fruta.speed(0)
+    nova_fruta.shape('square')
+    nova_fruta.color('green')
+    nova_fruta.penup()
+    velha_fruta.append(nova_fruta)
+    
 
-  #adicionando a bola a cobra
+  # adicionando a bola a cobra
   
-  for index in range(len(old_fruit)-1,0,-1):
-          a = old_fruit[index-1].xcor()
-          b = old_fruit[index-1].ycor()
+  for index in range(len(velha_fruta)-1,0,-1):
+    a = velha_fruta[index-1].xcor()
+    b = velha_fruta[index-1].ycor()
 
-          old_fruit[index].goto(a,b)
+    velha_fruta[index].goto(a,b)
                                 
-  if len(old_fruit)>0:
-          a= snake.xcor()
-          b = snake.ycor()
-          old_fruit[0].goto(a,b)
-  snake_move()
+  if len(velha_fruta)>0:
+    a= cobra.xcor()
+    b = cobra.ycor()
+    velha_fruta[0].goto(a,b)
+  cobra_move()
 
-  ##snake e a colisão da border   
-  if snake.xcor()>280 or snake.xcor()< -300 or snake.ycor()>240 or snake.ycor()<-240:
-          time.sleep(1)
-          screen.clear()
-          screen.bgcolor('turquoise')
-          scoring.goto(0,0)
-          scoring.write("   GAME OVER \n Sua pontuação foi {}".format(score),align="center",font=("Courier",30,"bold"))
+  ##cobra e a colisão da border   
+  if cobra.xcor()>280 or cobra.xcor()< -300 or cobra.ycor()>240 or cobra.ycor()<-240:
+    time.sleep(1)
+    screen.clear()
+    screen.bgcolor('turquoise')
+    pontuacao.goto(0,0)
+    pontuacao.write("   GAME OVER \n Sua pontuação foi {}".format(pontos),align="center",font=("Courier",30,"bold"))
 
 
   ## colisão da cobra
-  for food in old_fruit:
-          if food.distance(snake) < 20:
-            time.sleep(1)
-            screen.clear()
-            screen.bgcolor('turquoise')
-            scoring.goto(0,0)
-            scoring.write("    GAME OVER \n Sua pontuação foi {}".format(score),align="center",font=("Courier",30,"bold"))
+  for comida in velha_fruta:
+    if comida.distance(cobra) < 20:
+      time.sleep(1)
+      screen.clear()
+      screen.bgcolor('turquoise')
+      pontuacao.goto(0,0)
+      pontuacao.write("    GAME OVER \n Sua pontuação foi {}".format(pontos),align="center",font=("Courier",30,"bold"))
 
 
           
